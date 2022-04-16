a83764f6f08eb579605d2424ae2f1b14a39043ac
Akshay R R <101656424+akshay1027-altmoney@users.noreply.github.com>
2022-04-16 16:30:48 +0530

    queue in py using list

---
+++ python/queue.py
@@ -0,0 +1,28 @@
+queue = [1, 2, 3]
+
+
+def enqueue():
+    element = input('enter ele = ')
+    queue.append(element)
+
+
+def dequeue():
+    queue.pop(0)
+
+
+while True:
+    choice = int(input('enter the choice, 1. Push | 2. Pop | 3. Quit = '))
+
+    if choice == 1:
+        enqueue()
+        print(queue)
+
+    if choice == 2:
+        dequeue()
+        print(queue)
+
+    if choice == 3:
+        break
+
+    else:
+        print('Wrong choice')
