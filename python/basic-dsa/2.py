730e656074841c58189614f63935871a7a35c91e
Akshay R R <101656424+akshay1027-altmoney@users.noreply.github.com>
2022-04-24 22:07:24 +0530

    zoho intern questions

---
+++ python/basic-dsa/2.py
@@ -0,0 +1,22 @@
+# Move the zeros to last of the sequence
+
+# Exp :  1 2 0 3 0 4
+#         :  1 2 3 4 0
+
+num = []
+count_zero = 0
+n = int(input('enter the number of elements = '))
+
+for i in range(0, n):
+    ele = int(input())
+    if ele == 0:
+        count_zero += 1
+    else:
+        num.append(ele)
+
+num.sort()
+
+for i in range(0, count_zero):
+    num.append(0)
+
+print(num)
