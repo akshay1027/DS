801fbb428e3e3c1a9d7185e6e3d2baea8b26022d
Akshay R R <akshayar1027@gmail.com>
2022-04-25 14:11:41 +0530

    added algorithms and notes for the 4 problems

--- python/basic-dsa/2.py
+++ python/basic-dsa/2.py
@@ -3,6 +3,11 @@
 # Exp :  1 2 0 3 0 4
 #         :  1 2 3 4 0
 
+# algorithm:
+# 1) while getting input, count the number of zeros, dont append them in the array
+# 2) sort the array
+# 3) run a loop till total of zeros calculated in step 1 and append zeros in the array
+
 num = []
 count_zero = 0
 n = int(input('enter the number of elements = '))
