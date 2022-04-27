d83432afae6d089170520ab3bdfab2246d7fe0de
Akshay R R <akshayar1027@gmail.com>
2022-04-28 00:04:06 +0530

    duplicate py

--- python/basic-dsa/duplicate.py
+++ python/basic-dsa/duplicate.py
@@ -40,13 +40,11 @@ for i in range(n):
 
 nums.sort()
 
-1, 1, 2, 4
 for i in range(1, len(nums)):
     if nums[i] != nums[i-1]:
-        print('nums of j = ', nums[j], ' ** nums of i = ', nums[i])
+        # print('nums of j = ', nums[j], ' ** nums of i = ', nums[i])
         nums[j] = nums[i]
         j += 1
 
 for i in range(j):
     print(nums[i])
-# print(nums)
