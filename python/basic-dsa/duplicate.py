6e4499e6b8d75f51418031d1d82c93acf2b5ff4f
Akshay R R <akshayar1027@gmail.com>
2022-04-30 23:57:43 +0530

    duplicate py using hashmap and set DS

--- python/basic-dsa/duplicate.py
+++ python/basic-dsa/duplicate.py
@@ -32,19 +32,37 @@
 n = int(input('Enter n= '))
 
 nums = []
-j = 1  # will contain the place where duplicate variable is available
+j = 0  # will contain the place where duplicate variable is available
 
 for i in range(n):
     ele = int(input('ele = '))
     nums.append(ele)
 
-nums.sort()
+# nums.sort()
 
-for i in range(1, len(nums)):
-    if nums[i] != nums[i-1]:
-        # print('nums of j = ', nums[j], ' ** nums of i = ', nums[i])
+# for i in range(1, len(nums)):
+#     if nums[i] != nums[i-1]:
+#         # print('nums of j = ', nums[j], ' ** nums of i = ', nums[i])
+#         nums[j] = nums[i]
+#         j += 1
+
+# for i in range(j):
+#     print(nums[i])
+
+# solution 3:
+
+# res = set(nums)
+# print(res)
+
+# solution 4: using hashmap
+# Time complexity will be O(N) time and O(N) space
+mp = {i: 'a' for i in nums}
+
+for i in range(0, n):
+    if mp[nums[i]] == 'a':
         nums[j] = nums[i]
-        j += 1
+        j = j+1
+        mp[nums[i]] = 1
 
 for i in range(j):
     print(nums[i])
