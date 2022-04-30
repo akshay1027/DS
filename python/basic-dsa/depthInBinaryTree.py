bc78b258d07ce053a0cb1e7ad6bedaf1647a0809
Akshay R R <akshayar1027@gmail.com>
2022-05-01 02:55:08 +0530

    depth in binary tree, bfs and bfs recursive sol

---
+++ python/basic-dsa/depthInBinaryTree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # recursive BFS solution
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    # iterative BFS solution
+    def maxDepthBFS(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                level += 1
+
+        return level
