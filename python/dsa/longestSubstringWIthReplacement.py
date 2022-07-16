2c50fef9e323c0281e4dd4e06622c12bae6fee09
Akshay R R <akshayar1027@gmail.com>
2022-07-16 07:25:44 +0530

    longest substring with replacement

--- python/dsa/longestSubstringWIthReplacement.py
+++ python/dsa/longestSubstringWIthReplacement.py
@@ -6,11 +6,18 @@ class Solution:
         count = {}
         res = 0
 
+        # max(count.values()) => will give maximum value present in hashmap count
+
+        # left and right pointers to traverse across array
         left = 0
+
+        # stores maximum character frequency in the window
         maxf = 0
         for right in range(len(s)):
             # count.get(s[index], 0) will check if s[index] exists in hashmap count, if not will add it with value 0
             count[s[right]] = 1 + count.get(s[right], 0)
+
+            # set the maximum to max frequency of current character eventually you will get maxf.
             maxf = max(maxf, count[s[right]])
 
             if (right - left + 1) - maxf > k:
