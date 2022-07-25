50742c1694ad52b9fb5838698269d54e8786b58f
Akshay R R <akshayar1027@gmail.com>
2022-07-25 20:01:46 +0530

    valid anagram solution without sorted method

--- python/dsa/linkedlistcycle.py
+++ python/dsa/linkedlistcycle.py
@@ -5,6 +5,20 @@
 #         self.next = None
 
 class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+
+# hashmap solution, i did myself
+
+class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         hash = {}
         dummy = head
