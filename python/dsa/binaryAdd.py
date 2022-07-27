c314ca260e547f8000b1a6a799e5f7bbcb19c2ec
Akshay R R <akshayar1027@gmail.com>
2022-07-27 20:09:04 +0530

    binary add

--- python/dsa/binaryAdd.py
+++ python/dsa/binaryAdd.py
@@ -4,3 +4,19 @@
 
 # c++ solution
 # https://leetcode.com/problems/sum-of-two-integers/discuss/1394031/C%2B%2B-oror-100-faster-oror-With-explanation
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+
+        # 32 bit converting by masking with hexadecimal
+        mask = 0xffffffff
+
+        while b & mask > 0:
+            carry = (a & b) << 1
+            a = a ^ b
+            b = carry
+
+        if b > 0:
+            return (a & mask)
+        else:
+            return a
