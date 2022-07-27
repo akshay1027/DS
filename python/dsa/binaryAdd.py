7a78bfc584a3c3e3a770ff9b4677f853c847d67e
Akshay R R <akshayar1027@gmail.com>
2022-07-27 16:26:14 +0530

    adding few c++ solutions

---
+++ python/dsa/binaryAdd.py
@@ -0,0 +1,6 @@
+
+# python solution
+# https://leetcode.com/problems/sum-of-two-integers/discuss/489210/Read-this-if-you-want-to-learn-about-masks
+
+# c++ solution
+# https://leetcode.com/problems/sum-of-two-integers/discuss/1394031/C%2B%2B-oror-100-faster-oror-With-explanation
