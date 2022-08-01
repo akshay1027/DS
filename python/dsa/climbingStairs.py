3726f7a722e31f52332e4b8daa41280cb67190f7
Akshay R R <akshayar1027@gmail.com>
2022-08-01 11:11:09 +0530

    dp problems

--- python/dsa/climbingStairs.py
+++ python/dsa/climbingStairs.py
@@ -1,10 +1,23 @@
+# https://leetcode.com/problems/climbing-stairs/discuss/25313/Python-different-solutions-(bottom-up-top-down).
+
+from functools import lru_cache
+
+
+class Solution:
+    def __init__(self):
+        self.dic = {1: 1, 2: 2}
+
+    def climbStairs(self, n):
+        if n not in self.dic:
+            self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dic[n]
+
+
 # Making top-down not TLE using the lru_cache decorator.
 # Basically the lru_cache decorator will automatically cache the result with the same method call argument,
 # so climbStairs(x) will not be calculated more than once.
 # The parameter of the decorator is the max_size for the cache,
 # None means there is no size limit. This now will not TLE and beats 99.97%.
-
-from functools import lru_cache
 
 
 class Solution:
