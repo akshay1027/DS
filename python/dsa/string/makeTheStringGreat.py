e643b1edff52944d8b8d4b1ed00e11f1c722beb1
Akshay R R <akshayar1027@gmail.com>
2022-11-09 23:42:39 +0530

    make string great

---
+++ python/dsa/string/makeTheStringGreat.py
@@ -0,0 +1,10 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if stack and stack[-1] == c.swapcase():
+                stack.pop()
+            else:
+                stack.append(c)
+
+        return "".join(stack)
